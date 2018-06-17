86319323c51d03f9386a7433c613765a173d04f6
yo1000 <yoichi.kikuchi.aug@gmail.com>
2018-06-17 19:45:52 +0900

    Fix language from java to kotlin

---
+++ src/main/kotlin/com/yo1000/kc4boot2/Kc4Boot2Application.kt
@@ -0,0 +1,11 @@
+package com.yo1000.kc4boot2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Kc4Boot2Application
+
+fun main(args: Array<String>) {
+    runApplication<Kc4Boot2Application>(*args)
+}
---
+++ src/test/kotlin/com/yo1000/kc4boot2/Kc4Boot2ApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.yo1000.kc4boot2
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class Kc4Boot2ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
