bb5ebebf80f8fa505c8c41980220f8d4486bfc64
yo1000 <yoichi.kikuchi.aug@gmail.com>
2018-06-17 19:08:28 +0900

    Add minimal boot files

---
+++ src/main/java/com/yo1000/kc4boot2/Kc4Boot2Application.java
@@ -0,0 +1,12 @@
+package com.yo1000.kc4boot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kc4Boot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kc4Boot2Application.class, args);
+	}
+}
