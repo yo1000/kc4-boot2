f0bb62814d3d6aacdb1a7d3db5e563e6d1f49da1
yo1000 <yoichi.kikuchi.aug@gmail.com>
2018-06-18 03:27:08 +0900

    Save the state where it was confirmed that somehow run

--- src/main/kotlin/com/yo1000/kc4boot2/Kc4Boot2Application.kt
+++ src/main/kotlin/com/yo1000/kc4boot2/Kc4Boot2Application.kt
@@ -1,12 +1,24 @@
 package com.yo1000.kc4boot2
 
+import org.keycloak.adapters.AdapterDeploymentContext
+import org.keycloak.adapters.KeycloakConfigResolver
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver
+import org.keycloak.adapters.springsecurity.AdapterDeploymentContextFactoryBean
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
+import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter
+import org.keycloak.adapters.springsecurity.filter.KeycloakPreAuthActionsFilter
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
+import org.springframework.core.io.ClassPathResource
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
 import org.springframework.security.core.session.SessionRegistryImpl
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
@@ -38,13 +50,59 @@ class SecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
         return RegisterSessionAuthenticationStrategy(SessionRegistryImpl())
     }
 
+    @Bean
+    override fun adapterDeploymentContext(): AdapterDeploymentContext {
+        val factoryBean = AdapterDeploymentContextFactoryBean(ClassPathResource("keycloak.json"))
+        factoryBean.afterPropertiesSet()
+        return factoryBean.`object`!!
+    }
+
+    @Bean
+    fun grantedAuthoritiesMapper(): GrantedAuthoritiesMapper {
+        val mapper = SimpleAuthorityMapper()
+        mapper.setConvertToUpperCase(true)
+        return mapper
+    }
+
+    @Bean
+    fun keycloakConfigResolver(): KeycloakConfigResolver {
+        return KeycloakSpringBootConfigResolver()
+    }
+
+    @Bean
+    fun keycloakAuthenticationProcessingFilterRegistrationBean(
+            filter: KeycloakAuthenticationProcessingFilter): FilterRegistrationBean<KeycloakAuthenticationProcessingFilter> {
+        val registrationBean = FilterRegistrationBean(filter)
+        registrationBean.isEnabled = false
+        return registrationBean
+    }
+
+    @Bean
+    fun keycloakPreAuthActionsFilterRegistrationBean(
+            filter: KeycloakPreAuthActionsFilter): FilterRegistrationBean<KeycloakPreAuthActionsFilter> {
+        val registrationBean = FilterRegistrationBean(filter)
+        registrationBean.isEnabled = false
+        return registrationBean
+    }
+
+    override fun keycloakAuthenticationProvider(): KeycloakAuthenticationProvider {
+        val provider = super.keycloakAuthenticationProvider()
+        provider.setGrantedAuthoritiesMapper(grantedAuthoritiesMapper())
+        return provider
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder?) {
+        auth!!.authenticationProvider(keycloakAuthenticationProvider())
+    }
+
     override fun configure(http: HttpSecurity) {
         super.configure(http)
-        http
-                .authorizeRequests()
-                .antMatchers("/customers*").hasRole("USER")
-                .antMatchers("/admin*").hasRole("ADMIN")
-                .anyRequest().permitAll()
+        http.authorizeRequests()
+                .antMatchers("/sso/login*").permitAll()
+                .antMatchers("/customers*").hasAnyRole("user", "USER", "ROLE_USER")
+                .antMatchers("/admin*").hasAnyRole("admin", "ADMIN", "ROLE_ADMIN")
+                .anyRequest().permitAll();
+        http.csrf().disable()
     }
 }
 
@@ -56,7 +114,8 @@ class Kc4Controller {
     }
 
     @GetMapping("/admin")
-    fun getAdmin(): Any {
+    fun getAdmin(token: KeycloakAuthenticationToken): Any {
+        println(token)
         return "admin"
     }
 }
