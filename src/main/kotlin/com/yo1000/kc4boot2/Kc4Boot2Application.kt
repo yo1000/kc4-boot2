faba27b0362c3f6fc44de5d82ef31b9773d1d39e
yo1000 <yoichi.kikuchi.aug@gmail.com>
2018-06-17 23:59:59 +0900

    Add example code

--- src/main/kotlin/com/yo1000/kc4boot2/Kc4Boot2Application.kt
+++ src/main/kotlin/com/yo1000/kc4boot2/Kc4Boot2Application.kt
@@ -1,11 +1,62 @@
 package com.yo1000.kc4boot2
 
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.core.session.SessionRegistryImpl
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
 
 @SpringBootApplication
 class Kc4Boot2Application
 
 fun main(args: Array<String>) {
     runApplication<Kc4Boot2Application>(*args)
+}
+
+@KeycloakConfiguration
+class SecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
+    /**
+     * Registers the KeycloakAuthenticationProvider with the authentication manager.
+     */
+    fun configureGlobal(auth: AuthenticationManagerBuilder) {
+        auth.authenticationProvider(keycloakAuthenticationProvider())
+    }
+
+    /**
+     * Defines the session authentication strategy.
+     */
+    @Bean
+    override fun sessionAuthenticationStrategy(): SessionAuthenticationStrategy {
+        return RegisterSessionAuthenticationStrategy(SessionRegistryImpl())
+    }
+
+    override fun configure(http: HttpSecurity) {
+        super.configure(http)
+        http
+                .authorizeRequests()
+                .antMatchers("/customers*").hasRole("USER")
+                .antMatchers("/admin*").hasRole("ADMIN")
+                .anyRequest().permitAll()
+    }
+}
+
+@RestController
+class Kc4Controller {
+    @GetMapping("/customers")
+    fun getCustomers(): Any {
+        return "customers"
+    }
+
+    @GetMapping("/admin")
+    fun getAdmin(): Any {
+        return "admin"
+    }
 }
